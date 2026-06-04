0c99ee5ad715e047600980a09a79b4fc86325fe6
multiterra <tunahan.aydin2707@gmail.com>
2026-06-04 11:47:35 +0300

    First task Customer class is changed/adjusted.

--- customer_management/src/main/java/customermanagement/entity/Customer.java
+++ customer_management/src/main/java/customermanagement/entity/Customer.java
@@ -1,39 +1,23 @@
 package customermanagement.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.UUID;
 
 @Entity
 @Table(name = "customer_info",schema = "customers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Getter
-    @Setter
     private UUID customerId;
-
-    @Getter
-    @Setter
     private String name;
-    @Getter
-    @Setter
     private String surname;
-    @Getter
-    @Setter
     private String email;
-
-
-    public Customer() {
-    }
-
-    public Customer(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
-
 }
