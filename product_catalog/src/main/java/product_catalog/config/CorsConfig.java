53de45d1492575faaeb709a78cc620533aabbc53
multiterra <tunahan.aydin2707@gmail.com>
2026-06-24 18:32:06 +0300

    commit

---
+++ product_catalog/src/main/java/product_catalog/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package product_catalog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://multiterra-223h90w8v-tunahanaydin2707-8460s-projects.vercel.app"
+                        )
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
