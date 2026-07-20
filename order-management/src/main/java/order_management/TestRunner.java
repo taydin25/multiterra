7f1fee810dd989e29ed28a0f05ac0c12fc129df8
multiterra <tunahan.aydin2707@gmail.com>
2026-07-20 17:06:33 +0300

    commit last changes

--- order-management/src/main/java/order_management/TestRunner.java
+++ order-management/src/main/java/order_management/TestRunner.java
@@ -1,8 +1,8 @@
 package order_management;
 
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
-import order_management.repository.OrderRepository;
+//import lombok.Value;
+//import order_management.repository.OrderRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -10,16 +10,15 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class TestRunner implements CommandLineRunner {
 
-    private final OrderRepository orderRepository;
+//    private final OrderRepository orderRepository;
 
 //   @Value("${spring.data.mongodb.uri}")
 //   private String mongoUri;
 
     @Override
     public void run(String... args) {
-        System.out.println("COUNT = " + orderRepository.count());
-        System.out.println(orderRepository.findAll());
-
+ //       System.out.println("COUNT = " + orderRepository.count());
+        //System.out.println(orderRepository.findAll());
 //        System.out.println("MONGO URI = " + mongoUri);
     }
 }
